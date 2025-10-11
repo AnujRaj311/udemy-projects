afd7113d0f6abe83cc7cdb8b1a44ac0ff2397893
anuj.raj <araj1@ch.iitr.ac.in>
2025-10-11 19:42:37 +0530

    added setTimeout

--- project-05-weight-convertor/script.js
+++ project-05-weight-convertor/script.js
@@ -6,8 +6,14 @@ document.addEventListener('keydown', function(e) {
     const weightVal = Number(weight.value.trim());
 
     if (isNaN(weightVal) || weightVal <= 0) {
-      weightPounds.textContent = 'Please enter valid number.';
+      weightPounds.textContent = 'Please enter a valid number!.';
       weightPounds.classList.remove('hidden');
+      //setTimeout for reverting the original state
+      setTimeout(()=> {
+        weight.value = '';
+        weightPounds.classList.add('hidden');
+      }, 2500);
+
     } else {
       weightPounds.textContent = `Weight in pounds: ${(weightVal * 2.20462).toFixed(2)} lbs`;
       weightPounds.classList.remove('hidden');
